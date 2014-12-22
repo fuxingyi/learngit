be6520e68e46f88ca8e7badce170aeeaa17f7477
fuxingyi <zangbangxian@126.com>
2014-12-22 21:40:21 +0800

    myfirstpage

---
+++ myGitProject/src/myGitProject/zang/solo/com/MyFirstPage.java
@@ -0,0 +1,7 @@
+package myGitProject.zang.solo.com;
+
+public class MyFirstPage {
+	public static void main(String[] args) {
+		System.out.println("hello everyone , this my first git project !");
+	}
+}
